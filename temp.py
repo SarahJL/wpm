c99f2a2299b28a933f4431564f31a795e641acb2
Sarah Scott <sjlscott91@gmail.com>
2020-01-02 18:44:33 +0000

    testytest

--- temp.py
+++ temp.py
@@ -3,7 +3,6 @@ import curses
 def main(stdscr):
     curses.start_color()
     curses.use_default_colors()
-    stdscr.addstr('colours available: {}'.format(len(curses.COLORS)))
     for i in range(0, curses.COLORS):
         curses.init_pair(i + 1, i, -1)
     try:
